5d0b8471a9c700a0a548f5b2c26eb27e99425cf2
petter syvertsen <petter.syvertsen@gmail.com>
2020-09-18 07:53:23 +0200

    edit soon finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks', to: 'tasks#index' #show all 
+  get "tasks/new", to: 'tasks#new' #show form
+  
+  get 'tasks/:id', to: 'tasks#show', as: :task #show a specific task
+  post 'tasks', to: 'tasks#create' #create a new instance
+
+  get 'tasks/:id/edit', to: 'tasks#edit' #edit the task
   
 end
