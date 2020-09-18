28b2f9921be3b96ea174b5d1032d65a7c9e39135
petter syvertsen <petter.syvertsen@gmail.com>
2020-09-18 05:24:46 +0200

    second feature click and show

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,7 @@ ActiveRecord::Schema.define(version: 2020_09_18_021207) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
-    t.boolean "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
